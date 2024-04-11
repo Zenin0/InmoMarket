7c868c97d2b69dfecbf73a0b4615c9e338449ecb
Isaac Sanz <isaacsanz@proton.me>
2024-04-11 21:33:34 +0200

    [] -- Cambios

--- app/src/main/java/com/isanz/inmomarket/ui/portal/login/LoginActivity.kt
+++ app/src/main/java/com/isanz/inmomarket/ui/portal/login/LoginActivity.kt
@@ -5,7 +5,6 @@ import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
