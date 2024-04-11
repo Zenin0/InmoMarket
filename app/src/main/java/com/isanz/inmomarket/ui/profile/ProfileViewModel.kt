277d61cb498648e5c03feaa356504bb05a85102d
Isaac Sanz <isaacsanz@proton.me>
2024-04-11 22:04:06 +0200

    [] -- Perfil, mostrar nombre e imagen Boton para cambiar la contraseña

--- app/src/main/java/com/isanz/inmomarket/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/isanz/inmomarket/ui/profile/ProfileViewModel.kt
@@ -3,11 +3,20 @@ package com.isanz.inmomarket.ui.profile
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.isanz.inmomarket.InmoMarket
+import com.isanz.inmomarket.utils.entities.User
+import kotlinx.coroutines.tasks.await
 
 class ProfileViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is profile Fragment"
+    private val db = InmoMarket.getDb()
+
+    suspend fun retrieveProfile() : User {
+        val user = db.collection("users").document(InmoMarket.getAuth().currentUser!!.uid).get().await()
+        return user.toObject(User::class.java)!!
     }
-    val text: LiveData<String> = _text
+
+    fun resetPassword() {
+        InmoMarket.getAuth().sendPasswordResetEmail(InmoMarket.getAuth().currentUser!!.email!!)
+    }
 }
