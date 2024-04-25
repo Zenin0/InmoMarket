9d7157dcf2da3ae2a2bd721ac802202eb2366b12
Isaac Sanz <isaacsanz@proton.me>
2024-04-25 21:34:53 +0200

    [] -- Hacer el boton favoritos

--- app/src/main/java/com/isanz/inmomarket/utils/entities/Conversation.kt
+++ app/src/main/java/com/isanz/inmomarket/utils/entities/Conversation.kt
@@ -4,6 +4,6 @@ import com.google.firebase.database.Exclude
 
 data class Conversation(
     @Exclude val chatId: String = "",
-    val lastMessage: String = "",
+    val lastMessage: Message = Message(),
     val membersId: MutableList<String> = mutableListOf(),
 )
